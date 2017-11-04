06c286e1431884770eba811f9e8140f5b88b3957
Biel Cardona <bielcardona@gmail.com>
2017-11-04 21:38:33 +0100

    added built docs

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pySunScene',
+    version='0.9.1',
+    py_modules=['raytrace'],
+    url='',
+    license='MIT',
+    author='Gabriel Cardona, Ramon Pujol',
+    author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
+    description='',
+    install_requires=[
+        'numpy>=1',
+    ]
+)
