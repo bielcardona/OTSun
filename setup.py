09fe1fe74df4cb944a118528d8e9c4630bb258bb
Biel Cardona <bielcardona@gmail.com>
2018-02-10 21:51:43 +0100

    changed setup requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pySunScene',
-    version='0.9.5',
+    version='0.9.5b',
     py_modules=['raytrace'],
     url='',
+    install_requires=['numpy==1.12', 'enum', 'dill'],
     license='MIT',
     author='Gabriel Cardona, Ramon Pujol',
     author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
     description='',
-    install_requires=[
-        'numpy>=1',
-    ]
 )
+
