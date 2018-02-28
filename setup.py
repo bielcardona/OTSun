01359bb5bd4b7396e864df1a3ca223a1ce230846
Biel Cardona <bielcardona@gmail.com>
2018-02-28 12:11:44 +0100

    0.9.8.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pySunScene',
-    version='0.9.8.2',
+    version='0.9.8.3',
     py_modules=['raytrace'],
     url='',
     install_requires=['numpy==1.12', 'enum', 'dill'],
