afcdb02bf445ea3b856a80d0be2266e34ebdb899
Biel Cardona <bielcardona@gmail.com>
2019-05-07 10:00:33 +0200

    added log to test_2

--- tests/test_2.py
+++ tests/test_2.py
@@ -7,9 +7,30 @@ import Part
 import numpy as np
 import random
 random.seed(1)
+
+import logging
+logger = otsun.logger
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+
 # ---
 # Materials
 # ---
+
+
 
 MyProject = 'test_PTC.FCStd'
 FreeCAD.openDocument(MyProject)
