42cbf8055bb5a02c45904b43c3c75292c80cead4
Biel Cardona <bielcardona@gmail.com>
2019-07-21 11:49:52 +0200

    added info to setup

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=['otsun',],
-    url='',
+    url='https://github.com/bielcardona/OTSun',
     install_requires=['numpy==1.12', 'enum34' , 'autologging'],
     license='MIT',
     author='Gabriel Cardona, Ramon Pujol',
     author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
-    description='',
+    description='Analizer of sun collectors',
 )
 
