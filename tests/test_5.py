2d92d147211c9aa9a5e81bb667b02cd5764859c9
Biel Cardona <bielcardona@gmail.com>
2019-08-07 18:31:24 +0000

    changed print to function

--- tests/test_5.py
+++ tests/test_5.py
@@ -109,8 +109,8 @@ for ph in np.arange(phi_ini, phi_end, phi_step):
 
 FreeCAD.closeDocument(FreeCAD.ActiveDocument.Name)
 
-print results
-print 0.9 > results[0][2] > 0.7 and 0.85 > results[1][2] > 0.5 and results[0][3] == 0.0 and results[1][3] == 0.0
+print (results)
+print (0.9 > results[0][2] > 0.7 and 0.85 > results[1][2] > 0.5 and results[0][3] == 0.0 and results[1][3] == 0.0)
 
 def test_5():
     assert 0.9 > results[0][2] > 0.7 and 0.85 > results[1][2] > 0.5 and results[0][3] == 0.0 and results[1][3] == 0.0
