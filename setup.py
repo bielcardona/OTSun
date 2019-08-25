c885c010c08a274e7b87ba27e05a9b863ae9b2d5
Biel Cardona <bielcardona@gmail.com>
2019-08-25 23:04:02 +0200

    updated config.py with dependencies for python 3.* and 2.7

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     packages=['otsun',],
     url='https://github.com/bielcardona/OTSun',
-    install_requires=['numpy==1.12', 'enum34' , 'autologging'],
+    install_requires=['numpy==1.12' , 'autologging'],
+    extras_require ={':python_version == "2.7"': [
+            'enum34', 'backports.functools_lru_cache'
+        ],
+    },
     license='MIT',
     author='Gabriel Cardona, Ramon Pujol',
     author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
