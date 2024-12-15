e4e1afe2f218ab92bf2319553ed369d379c1eda2
Biel Cardona <bielcardona@gmail.com>
2024-12-15 22:13:56 +0100

    moved to poetry

--- src/otsun/__init__.py
+++ src/otsun/__init__.py
@@ -12,8 +12,26 @@ from .source import *
 from .scene import *
 from .experiments import *
 from .movements import *
-from .__about__ import __version__
+
+import importlib.metadata
+from pathlib import Path
+import toml
 
 import logging
 logger = logging.getLogger(__name__)
 
+try:
+    __version__ = importlib.metadata.version('otsun')
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+    # adopt path to your pyproject.toml
+    pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
+    print(pyproject_toml_file)
+    if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
+        data = toml.load(pyproject_toml_file)
+        # check project.version
+        if "project" in data and "version" in data["project"]:
+            __version__ = data["project"]["version"]
+        # check tool.poetry.version
+        elif "tool" in data and "poetry" in data["tool"] and "version" in data["tool"]["poetry"]:
+            __version__ = data["tool"]["poetry"]["version"]
